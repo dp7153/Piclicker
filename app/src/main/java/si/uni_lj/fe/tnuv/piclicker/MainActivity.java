3326099c39aa92151d018863fbc31791be7d0743
Luka <lp2499@student.uni-lj.si>
2024-05-09 10:07:00 +0200

    click button zamenjan za image

--- app/src/main/java/si/uni_lj/fe/tnuv/piclicker/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/piclicker/MainActivity.java
@@ -1,11 +1,11 @@
 package si.uni_lj.fe.tnuv.piclicker;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnClick = findViewById(R.id.btn_click);
+        ImageButton btnClick = findViewById(R.id.btn_click);
         textViewCookies = findViewById(R.id.click);
 
         // Load cookies count from SharedPreferences
@@ -27,23 +27,20 @@ public class MainActivity extends AppCompatActivity {
         cookies = preferences.getInt(COOKIES_KEY, 0);
         updateCookiesDisplay();
 
-        btnClick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Increase the number of cookies and update the TextView
-                cookies++;
-                updateCookiesDisplay();
+        btnClick.setOnClickListener(v -> {
+            // Increase the number of cookies and update the TextView
+            cookies++;
+            updateCookiesDisplay();
 
-                // Save cookies count to SharedPreferences
-                SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
-                editor.putInt(COOKIES_KEY, cookies);
-                editor.apply();
-            }
+            // Save cookies count to SharedPreferences
+            SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+            editor.putInt(COOKIES_KEY, cookies);
+            editor.apply();
         });
     }
 
     // Update the TextView to display the current number of cookies
     private void updateCookiesDisplay() {
-        textViewCookies.setText("Cookies: " + cookies);
+        textViewCookies.setText("Clicks: " + cookies);
     }
 }
